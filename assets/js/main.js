0f6a3d8db201bbe893a3c2ed35f342cab64608ff
Dale Stephenson <dalestephenson@zohomail.com>
2021-05-25 21:10:17 +1200

    slider test

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,37 +255,20 @@
 
 		}
 
-})(jQuery);
+	})(jQuery);
 
-// skills dropdown
-	var coll = document.getElementsByClassName("collapsible");
-	var i;
+		// skills dropdown
+			var coll = document.getElementsByClassName("collapsible");
+			var i;
 
-	for (i = 0; i < coll.length; i++) {
-	coll[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if (content.style.display === "block") {
-		content.style.display = "none";
-		} else {
-		content.style.display = "block";
-		}
-	});
-
-
-// education slider
-	var myIndex = 0;
-	carousel();
-
-	function carousel() {
-	var i;
-	var x = document.getElementsByClassName("mySlides");
-	for (i = 0; i < x.length; i++) {
-		x[i].style.display = "none";  
-	}
-	myIndex++;
-	if (myIndex > x.length) {myIndex = 1}    
-	x[myIndex-1].style.display = "block";  
-	setTimeout(carousel, 4000); // Change image every 4 seconds
-	}
-} 
+			for (i = 0; i < coll.length; i++) {
+			coll[i].addEventListener("click", function() {
+				this.classList.toggle("active");
+				var content = this.nextElementSibling;
+				if (content.style.display === "block") {
+				content.style.display = "none";
+				} else {
+				content.style.display = "block";
+				}
+			});
+	} 
