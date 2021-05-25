a77533745738767c70e64acea939884dbfb64baa
Dale Stephenson <dalestephenson@zohomail.com>
2021-05-25 21:06:56 +1200

    slider test

--- assets/js/main.js
+++ assets/js/main.js
@@ -274,19 +274,18 @@
 
 
 // education slider
-	// var slideIndex = 0;
-	// carousel();
-	
-	// function carousel() {
-	//   var i;
-	//   var x = document.getElementsByClassName("mySlides");
-	//   for (i = 0; i < x.length; i++) {
-	// 	x[i].style.display = "none"; 
-	//   }
-	//   slideIndex++;
-	//   if (slideIndex > x.length) {slideIndex = 1} 
-	//   x[slideIndex-1].style.display = "block"; 
-	//   setTimeout(carousel, 2000); // Change image every 2 seconds
-	// };
-	
+	var myIndex = 0;
+	carousel();
+
+	function carousel() {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";  
+	}
+	myIndex++;
+	if (myIndex > x.length) {myIndex = 1}    
+	x[myIndex-1].style.display = "block";  
+	setTimeout(carousel, 4000); // Change image every 4 seconds
+	};
 } 
